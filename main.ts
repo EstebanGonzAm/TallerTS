dba697c2e52fe646689d6b0e3c27cbf633633e18
EstebanGonzAm <69725204+EstebanGonzAm@users.noreply.github.com>
2022-10-24 12:18:45 -0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -4,37 +4,74 @@ import { dataSeries } from './data.js';
 
 let seriesTable: HTMLElement = document.getElementById("series")!;
 
+let ids: HTMLElement = document.getElementById("ids")!;
+
+let tarjeta: HTMLElement = document.getElementById("tarjeta")!;
+
+let nombre: HTMLElement = document.getElementById("nombre")!;
+
+let canales: HTMLElement = document.getElementById("channel")!;
+
+let seasons: HTMLElement = document.getElementById("seasons")!;
+
 let promedioNumero: HTMLElement = document.getElementById("promedio")!;
+
+const boton_serie = document.getElementById('nombre')!;
 
 renderSeriesInTable(dataSeries);
 calcularPromedioTemporadas(dataSeries);
 
-
 function renderSeriesInTable(series: Serie[]): void {
     series.forEach(c => {
-      let trElement = document.createElement("tr");
-      trElement.innerHTML = `<td style="font-weight: bold;">${c.id}</td>
-                             <td>${c.name}</td>
-                             <td>${c.channel}</td>
-                             <td>${c.seasons}</td>`;
-      seriesTable.appendChild(trElement);
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td style="font-weight: bold;">${c.id}</td>`;
+        const btnElement = document.createElement("tr");
+          btnElement.onclick = () => mostrarTarjeta(c);
+          btnElement.innerHTML=`<td>${c.name}</td>`;
+        let trElement3 = document.createElement("tr");
+        trElement3.innerHTML = `<td>${c.channel}</td>`;
+        let trElement4 = document.createElement("tr");
+        trElement4.innerHTML = `<td>${c.seasons}</td>`;
+      ids.appendChild(trElement);
+      nombre.appendChild(btnElement);
+      canales.appendChild(trElement3);
+      seasons.appendChild(trElement4);
     });
   }
 
-  //let btnSerie: HTMLElement = document.getElementById("boton_serie")!;
-  //btnSerie.onclick = muestraAlerta(1);
-    
-//   function muestraAlerta(numero: number) {
-//     console.log(numero);
-//   }
 
+  function mostrarTarjeta(serie: Serie): void{
+
+    let colInfo: HTMLElement = document.getElementById("tarjeta")!;
+    let cardAc = document.getElementById("cardId");
+  
+    if (cardAc != null){
+      colInfo.removeChild(cardAc);
+    }
+    
+    let cardElement: HTMLElement = document.createElement("div");
+    let cardBody: HTMLElement = document.createElement("div");
+  
+    cardElement.className = "card";
+    cardElement.id = "cardId"
+    cardElement.setAttribute("style", "width: 18rem;");
+    cardBody.className = "card-body";
+    cardElement.innerHTML = `<img class="card-img-top" src=${serie.image}>`;
+    cardBody.innerHTML = `<h4 class="card-title"> ${serie.name} </h4>
+                          <p class="card-text"> ${serie.description} </p>
+                          <a href= ${serie.link}> ${serie.link} </a>`;
+  
+    cardElement.appendChild(cardBody);
+    colInfo.appendChild(cardElement);
+
+  }
+  
   function calcularPromedioTemporadas(series: Serie[]): void{
       let promedio = 0;
       let numSeries = series.length;
       series.forEach(c=> {
           promedio+= c.seasons
-          
-      })
+        })
       let trElement = document.createElement("tr");
       promedio = promedio/numSeries;
       console.log(promedio);
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,48 @@
 import { dataSeries } from './data.js';
 var seriesTable = document.getElementById("series");
+var ids = document.getElementById("ids");
+var tarjeta = document.getElementById("tarjeta");
+var nombre = document.getElementById("nombre");
+var canales = document.getElementById("channel");
+var seasons = document.getElementById("seasons");
 var promedioNumero = document.getElementById("promedio");
+var boton_serie = document.getElementById('nombre');
 renderSeriesInTable(dataSeries);
 calcularPromedioTemporadas(dataSeries);
 function renderSeriesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td style=\"font-weight: bold;\">".concat(c.id, "</td>\n                             <td>").concat(c.name, "</td>\n                             <td>").concat(c.channel, "</td>\n                             <td>").concat(c.seasons, "</td>");
-        seriesTable.appendChild(trElement);
+        trElement.innerHTML = "<td style=\"font-weight: bold;\">".concat(c.id, "</td>");
+        var btnElement = document.createElement("tr");
+        btnElement.onclick = function () { return mostrarTarjeta(c); };
+        btnElement.innerHTML = "<td>".concat(c.name, "</td>");
+        var trElement3 = document.createElement("tr");
+        trElement3.innerHTML = "<td>".concat(c.channel, "</td>");
+        var trElement4 = document.createElement("tr");
+        trElement4.innerHTML = "<td>".concat(c.seasons, "</td>");
+        ids.appendChild(trElement);
+        nombre.appendChild(btnElement);
+        canales.appendChild(trElement3);
+        seasons.appendChild(trElement4);
     });
 }
-//let btnSerie: HTMLElement = document.getElementById("boton_serie")!;
-//btnSerie.onclick = muestraAlerta(1);
-//   function muestraAlerta(numero: number) {
-//     console.log(numero);
-//   }
+function mostrarTarjeta(serie) {
+    var colInfo = document.getElementById("tarjeta");
+    var cardAc = document.getElementById("cardId");
+    if (cardAc != null) {
+        colInfo.removeChild(cardAc);
+    }
+    var cardElement = document.createElement("div");
+    var cardBody = document.createElement("div");
+    cardElement.className = "card";
+    cardElement.id = "cardId";
+    cardElement.setAttribute("style", "width: 18rem;");
+    cardBody.className = "card-body";
+    cardElement.innerHTML = "<img class=\"card-img-top\" src=".concat(serie.image, ">");
+    cardBody.innerHTML = "<h4 class=\"card-title\"> ".concat(serie.name, " </h4>\n                          <p class=\"card-text\"> ").concat(serie.description, " </p>\n                          <a href= ").concat(serie.link, "> ").concat(serie.link, " </a>");
+    cardElement.appendChild(cardBody);
+    colInfo.appendChild(cardElement);
+}
 function calcularPromedioTemporadas(series) {
     var promedio = 0;
     var numSeries = series.length;
